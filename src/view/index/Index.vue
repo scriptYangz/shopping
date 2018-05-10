e8e35516c9b73f63ac26a6c111e843b63b69a7b6
scriptYangz <scriptYangz163.com>
2018-05-10 14:51:04 +0800

    1.01

---
+++ src/view/index/Index.vue
@@ -0,0 +1,635 @@
+<template>
+	<div id="index">
+		<div class="header">
+			<div class="header-banner">
+				<img src="../../../static/img/index-banner.png" />
+			</div>
+			<div class="con">
+
+			</div>
+
+			<div class="header-box">
+
+				<div class="header-cities fl">
+					<font>广州</font>&nbsp<span><img src="../../../static/img/index-city.png"/></span>
+				</div>
+				<div class="header-search fr">
+					<span><img src="../../../static/img/index-searchicon.png" /></span>
+					<input type="text" id="search" value="" placeholder="输入关键词" />
+				</div>
+				<div class="header-msg fr">
+					<img src="../../../static/img/index-msg.png" />
+				</div>
+			</div>
+
+		</div>
+
+		<div class="main">
+			<div class="main-top">
+				<div class="main-top1 fl">
+					<img src="../../../static/img/index-icon01.png" />
+					<span>推荐商品</span>
+				</div>
+				<div class="main-top2 fl">
+					<img src="../../../static/img/index-icon02.png" />
+					<span>新品上市</span>
+				</div>
+				<div class="main-top3 fl">
+					<img src="../../../static/img/index-icon03.png" />
+					<span>热门商品</span>
+				</div>
+				<div class="main-top4 fl">
+					<img src="../../../static/img/index-icon04.png" />
+					<span>优惠促销</span>
+				</div>
+				<div class="main-top5 fl">
+					<img src="../../../static/img/index-icon05.png" />
+					<span>所有商品</span>
+				</div>
+			</div>
+			<div class="main-discount">
+				<img src="../../../static/img/index-bigImage1.png" />
+			</div>
+			<div class="recom">
+				<div class="recom-title">
+					<div class="recom-title-content">
+						<img src="../../../static/img/index-icon01.png" />
+						<span>推荐商品</span>
+					</div>
+
+				</div>
+				<div class="recom-content">
+					<div class="recom-left fl">
+						<div class="recom1">
+							<p class="black">欧式室内房门锁套餐</p>
+							<p class="r1">精工实心 姿态大不一样</p>
+						</div>
+						<div class="manjian">
+							<span>满99-20</span>
+						</div>
+						<img src="../../../static/img/index-recom1.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="recom-right fr">
+						<div class="recom-right-top">
+							<div class="recom2">
+								<p class="black">欧式象牙白双用门吸</p>
+								<p class="r2">平开合页 承重能力强</p>
+							</div>
+							<img src="../../../static/img/index-recom2.png" />
+						</div>
+						<div class="recom-right-bottom">
+							<div class="recom3">
+								<p class="black">空转C级防盗门锁芯</p>
+								<p class="r3">限时特惠 最后一波</p>
+							</div>
+							<img src="../../../static/img/index-recom3.png" />
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="bigImage2">
+				<img src="../../../static/img/index-bigImage2.png" />
+			</div>
+
+			<div class="list">
+				<div class="list-title">
+					<div class="list-title-content">
+						<img src="../../../static/img/index-icon02.png" />
+						<span>新品推荐</span>
+						<a href="">更多</a>
+					</div>
+				</div>
+				<div class="list-content">
+					<div class="list-content-1">
+						<img src="../../../static/img/index-class-image1.png" />
+
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-2">
+						<img src="../../../static/img/index-class-image2.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-3">
+						<img src="../../../static/img/index-class-image3.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-4">
+						<img src="../../../static/img/index-class-image4.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="list">
+				<div class="list-title">
+					<div class="list-title-content">
+						<img src="../../../static/img/index-icon03.png" />
+						<span>热门商品</span>
+						<a href="">更多</a>
+					</div>
+				</div>
+				<div class="list-content">
+					<div class="list-content-1">
+						<img src="../../../static/img/index-class-image1.png" />
+
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-2">
+						<img src="../../../static/img/index-class-image2.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-3">
+						<img src="../../../static/img/index-class-image3.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-4">
+						<img src="../../../static/img/index-class-image4.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="list">
+				<div class="list-title">
+					<div class="list-title-content">
+						<img src="../../../static/img/index-icon04.png" />
+						<span>优惠促销</span>
+						<a href="">更多</a>
+					</div>
+				</div>
+				<div class="list-content">
+					<div class="list-content-1">
+						<img src="../../../static/img/index-class-image1.png" />
+
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-2">
+						<img src="../../../static/img/index-class-image2.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-3">
+						<img src="../../../static/img/index-class-image3.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-4">
+						<img src="../../../static/img/index-class-image4.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="list">
+				<div class="list-title">
+					<div class="list-title-content">
+						<img src="../../../static/img/index-icon05.png" />
+						<span>所有商品</span>
+						<a href="">更多</a>
+					</div>
+				</div>
+				<div class="list-content">
+					<div class="list-content-1">
+						<img src="../../../static/img/index-class-image1.png" />
+
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-2">
+						<img src="../../../static/img/index-class-image2.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-3">
+						<img src="../../../static/img/index-class-image3.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+					<div class="list-content-4">
+						<img src="../../../static/img/index-class-image4.png" />
+						<div class="price">
+							<span>￥189</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="footer">
+			<div class="tab-index fl">
+				<img src="../../../static/img/icon-index-select.png" />
+				<span>首页</span>
+			</div>
+			<div class="tab-class fl">
+				<img src="../../../static/img/icon-class.png" />
+				<span>分类</span>
+			</div>
+			<div class="tab-special fl">
+				<img src="../../../static/img/icon-special.png" />
+				<span>专题</span>
+			</div>
+			<div class="tab-cart fl">
+				<img src="../../../static/img/icon-cart.png" />
+				<span>购物车</span>
+			</div>
+			<div class="tab-personal fl">
+				<img src="../../../static/img/icon-personal.png" />
+				<span>个人</span>
+			</div>
+		</div>
+	</div>
+</template>
+ 
+ 
+<script>
+	export default {
+		name: 'Index',
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+
+
+
+@import '../../../static/css/golbal.css';
+<style scoped rel="stylesheet">
+   .header {
+				position: relative;
+				padding-bottom: 50%;
+			}
+			
+			.header-banner {
+				position: absolute;
+				top: 0;
+				width: 100%;
+			}
+			
+			.header-banner img {
+				width: 100%;
+			}
+			
+			.con {
+				position: absolute;
+				top: 0;
+				width: 100%;
+				height: 25%;
+				background: linear-gradient(rgba(103, 103, 103, 0.5), rgba(103, 103, 103, 0));
+			}
+			
+			.header-box {
+				width: 100%;
+				color: white;
+				position: absolute;
+				top: 0;
+			}
+			
+			.header-box img {
+				vertical-align: middle;
+			}
+			
+			.header-cities {
+				font-family: "宋体";
+				font-size: 50%;
+				width: 25%;
+				display: inline-block;
+				text-align: center;
+				position: absolute;
+				top: 50%;
+				transform: translateY(-50%);
+			}
+			
+			.header-cities span {
+				display: inline-block;
+			}
+			
+			.header-cities img {
+				position: absolute;
+				top: 50%;
+				transform: translateY(-50%);
+				height: 80%;
+			}
+			
+			.header-search {
+				margin: 0 auto;
+				display: inline-block;
+				height: 100%;
+				padding: 1% 0;
+				width: 62%;
+				position: absolute;
+				top: 50%;
+				left: 25%;
+				transform: translateY(-50%);
+			}
+			
+			.header-search img {
+				position: absolute;
+				top: 50%;
+				left: 6%;
+				transform: translateY(-50%);
+				height: 75%;
+			}
+			
+			#search {
+				position: absolute;
+				top: 50%;
+				transform: translateY(-50%);
+				width: 100%;
+				height: 100%;
+				/*height: 43px;*/
+				opacity: 0.4;
+				border-radius: 30px;
+				background-color: #dfdfdf;
+				text-indent: 16%;
+				/*opacity: 0.3;*/
+			}
+			
+			.header-msg {
+				display: inline-block;
+			}
+			
+			.header-msg img {
+				display: block;
+				vertical-align: middle;
+				width: 50%;
+				margin: 0 auto;
+			}
+			
+			.main {
+				background-color: #F8F8F8;
+				margin-bottom: 80px;
+			}
+			
+			.main-top {
+				background-color: white;
+			}
+			
+			.main-top div {
+				background-color: white;
+				display: inline-block;
+				width: 20%;
+				text-align: center;
+				font-family: "宋体";
+				font-size: 50%;
+				color: #999999;
+				padding-bottom: 10%;
+				padding-top: 6%;
+			}
+			
+			.main-top div:hover {
+				cursor: pointer;
+				color: red;
+			}
+			
+			.main-top img {
+				display: block;
+				margin: 0 auto;
+				vertical-align: middle;
+				width: 50%;
+				padding-bottom: 10px;
+			}
+			
+			.main-discount {
+				width: 100%;
+			}
+			
+			.main-discount img {
+				width: 100%;
+				margin-top: 4%;
+				margin-bottom: 4%;
+			}
+			
+			.recom {
+				overflow: hidden;
+				position: relative;
+				background-color: white;
+				border-bottom: solid #e5e5e5 1px;
+			}
+			
+			.recom-content,
+			.recom-title {
+				width: 100%;
+			}
+			
+			.recom-title {
+				height: 100%;
+				text-align: center;
+				position: relative;
+				border-bottom: solid #e5e5e5 1px;
+			}
+			
+			.recom-title-content {
+				padding: 4% 0;
+				width: 60%;
+				margin: 0 auto;
+			}
+			
+			.recom-title span {
+				vertical-align: middle;
+			}
+			
+			.recom-title img {
+				width: 12%;
+				vertical-align: middle;
+			}
+			
+			.recom .r1 {
+				color: #ff9f4a;
+			}
+			
+			.recom .r2 {
+				color: #00aeff;
+			}
+			
+			.recom .r3 {
+				color: #4fbf74;
+			}
+			
+			.recom .black {
+				color: #666666;
+			}
+			
+			.recom p {
+				padding-left: 8%;
+			}
+			
+			.price {
+				margin: 10% auto;
+				color: #c22820;
+				text-align: center;
+			}
+			
+			.manjian {
+				margin-left: 8%;
+				width: 40%;
+				height: 25%;
+				text-align: center;
+				background: -moz-linear-gradient(left, #ff403b, #ffa721);
+				/*Mozilla*/
+				background: -webkit-gradient(linear, 0 50%, 100% 50%, from(#ff403b), to(#ffa721));
+				/*Old gradient for webkit*/
+				background: -webkit-linear-gradient(left, #ff403b, #ffa721);
+				/*new gradient for Webkit*/
+				background: -o-linear-gradient(left, #ff403b, #ffa721);
+				/*Opera11*/
+				color: white;
+			}
+			
+			.recom-left {
+				display: inline-block;
+				width: 50%;
+				height: 100%;
+				border-right: solid #e5e5e5 1px;
+			}
+			
+			.recom-left img {
+				width: 100%;
+				padding: 10% 0;
+			}
+			
+			.recom1,.recom2,.recom3{
+				padding:10% 0 ;
+			}
+			
+			.recom-right {
+				width: 49%;
+				height: 100%;
+				display: inline-block;
+			}
+			
+			.recom-right div {
+				width: 100%;
+				display: block;
+			}
+			
+			.recom-right div img {
+				width: 100%;
+				display: block;
+			}
+			
+			.recom-right-top {
+				border-bottom: solid #e5e5e5 1px;
+			}
+			
+			.bigImage2 {
+				background-color: white;
+				width: 100%;
+				padding-top: 6%;
+			}
+			
+			.bigImage2 img {
+				width: 100%;
+			}
+			
+			
+			
+			.list{
+				margin-top: 6%;
+			}
+			.list-title {
+				background-color: white;
+			}
+			
+			.list-title-content {
+				padding: 4% 0;
+				width: 60%;
+				margin: 0 2%;
+			}
+			
+			.list-title span {
+				vertical-align: middle;
+			}
+			
+			.list-title img {
+				width: 12%;
+				vertical-align: middle;
+			}
+			
+			.list-title a {
+				position: absolute;
+				right: 2%;
+				width: 12%;
+				vertical-align: middle;
+				text-decoration: none;
+				color: #cccccc;
+			}
+			
+			.list-content {
+				display: flex;
+				flex-wrap: wrap;
+				background-color: white;
+				
+				/*wrap和wrap-reverse。*/
+			}
+			
+			.list-content>div {
+				width: 49%;
+				border-bottom: solid #e5e5e5 1px;
+			}
+			
+			.list-content img {
+				margin: 0 auto;
+				width: 100%;
+			}
+			
+			.list-content>div:nth-child(odd) {
+				border-right: solid #e5e5e5 1px;
+			}
+			
+			.footer {
+				position:  fixed;
+				bottom: 0;
+				background-color: white;
+				border-top: solid #e5e5e5 1px;
+				width: 100%;
+			}
+			
+			.footer div {
+				display: inline-block;
+				width: 20%;
+				text-align: center;
+				font-family: "宋体";
+				font-size: 50%;
+				color: #999999;
+				padding-bottom: 2%;
+			}
+			
+			.footer div img {
+				display: block;
+				margin: 0 auto;
+				width: 28%;
+				padding-bottom: 8%;
+				padding-top: 20%;
+			}
+</style>
